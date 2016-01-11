a3a09157c3a5890ef2aa6465d48721b49730dd09
Christine Tacha and Gracie Lundell <tacha.christine@gmail.com>
2016-01-11 15:32:05 -0800

    style webpage

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("#title").click(function() {
+    $("#info").toggle();
+  });
+
+  $("#modulo").click(function() {
+    $("#modulo-info").toggle();
+  });
+
+  $("#operators").click(function() {
+    $("#operators-info").toggle();
+  });
+
+  $("#nan").click(function() {
+    $("#nan-info").toggle();
+  });
+
+});
